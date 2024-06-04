e7c19fb5ad211bb5e72ad14c539670aa9cfadc55
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-06-04 14:23:46 +0200

    Publish `Identified`.

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -2,7 +2,28 @@
 pub use term::simple::SimpleMinimalTerm;
 pub use term::AltTermIdAware;
 pub use term::{MinimalTerm, Term};
-pub use term_id::{Identified, TermId};
+pub use term_id::TermId;
 
 mod term;
 mod term_id;
+#[cfg(feature="pyo3")]
+pub(crate) mod py;
+
+
+/// `Identified` is implemented by entities that have a [`TermId`] as an identifier.
+///
+/// ## Examples
+///
+/// [`crate::base::SimpleMinimalTerm`] implements `Identified`.
+/// ```
+/// use ontolius::prelude::*;
+/// use ontolius::base::SimpleMinimalTerm;
+///
+/// let term_id = TermId::from(("HP", "1234567"));
+/// let term = SimpleMinimalTerm::new(term_id, "Sample term", vec![], false);
+///
+/// assert_eq!(term.identifier().to_string(), "HP:1234567")
+/// ```
+pub trait Identified {
+    fn identifier(&self) -> &TermId;
+}
